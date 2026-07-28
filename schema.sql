2e8954d3791c8de2ba60c5b7d3efe1cebbaf3c46
Cameron Lee <cameronkmlee10@gmail.com>
2026-07-27 22:10:10 -0400

    Update schema

--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@ CREATE TABLE IF NOT EXISTS prs (
     user_id INTEGER NOT NULL,
     name TEXT NOT NULL,
     pr_link TEXT NOT NULL,
-    onboarding_type TEXT CHECK (onboarding_type in ('Firmware', 'GS')),
+    onboarding_type TEXT CHECK (onboarding_type in ('Firmware', 'Software')),
     notion_email TEXT NOT NULL,
     status TEXT CHECK (status in ('Pending', 'Approved')) DEFAULT 'Pending',
     submitted_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
